03db9b5ef2059bbfe25de402d98b6577a08144c6
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-09-28 23:03:15 +0000

    Base project
    Basic camera controls, no mouse movement. Pyramid object.

---
+++ Assignment 3/core/src/com/ru/tgra/shapes/Camera.java
@@ -0,0 +1,75 @@
+package com.ru.tgra.shapes;
+
+import java.nio.FloatBuffer;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.BufferUtils;
+
+public class Camera {
+	
+	Point3D eye;
+	Vector3D u, v, n;
+	
+	private FloatBuffer matrixBuffer;
+	
+	public Camera(Point3D _eye) {
+		eye = _eye;
+		u = new Vector3D(0,0,0);
+		v = new Vector3D(0,0,0);
+		n = new Vector3D(0,0,0);
+	}
+	
+	public void Look3D(Point3D eye, Point3D center, Vector3D up) {
+		this.n = Vector3D.difference(eye, center);
+		this.u = up.cross(n);
+		n.normalize();
+		u.normalize();
+		this.v = n.cross(u);
+	}
+	
+	public void Slide(float delU, float delV, float delN) {
+		eye.x += delU * u.x + delV * v.x + delN * n.x;
+		eye.y += delU * u.y + delV * v.y + delN * n.y;
+		eye.z += delU * u.z + delV * v.z + delN * n.z;
+	}
+	
+	public void RotateAxis(Vector3D a, Vector3D b, float angle) {
+		float ang = (float) (Math.PI/180 * angle);
+		float C = (float) Math.cos(ang), S = (float) Math.sin(ang);
+		
+		Vector3D t = new Vector3D(C * a.x + S * b.x, C * a.y + S * b.y, C * a.z + S * b.z);
+		b.set(-S * a.x + C * b.x, -S * a.y + C * b.y, -S * a.z + C * b.z);
+		a.set(t.x, t.y, t.z);
+		
+	}
+	
+	public void Roll(float angle)
+	{
+		RotateAxis( u, v, angle );
+	}
+	
+	public void Pitch(float angle) {
+		
+		RotateAxis( n, v, angle );
+	}
+	public void Yaw(float angle) {
+		
+		RotateAxis( u, n, angle );
+	}
+	public void setShaderMatrix(int viewMatrixLoc) {
+		Vector3D minusEye = new Vector3D(-eye.x, -eye.y, -eye.z);
+		
+		float[] pm = new float[16];
+
+		pm[0] = u.x; pm[4] = u.y; pm[8] = u.z; pm[12] = minusEye.dot(u);
+		pm[1] = v.x; pm[5] = v.y; pm[9] = v.z; pm[13] = minusEye.dot(v);
+		pm[2] = n.x; pm[6] = n.y; pm[10] = n.z; pm[14] = minusEye.dot(n);
+		pm[3] = 0.0f; pm[7] = 0.0f; pm[11] = 0.0f; pm[15] = 1.0f;
+
+		matrixBuffer = BufferUtils.newFloatBuffer(16);
+		matrixBuffer.put(pm);
+		matrixBuffer.rewind();
+		Gdx.gl.glUniformMatrix4fv(viewMatrixLoc, 1, false, matrixBuffer);
+	}
+
+}
---
+++ Assignment 3/core/src/com/ru/tgra/shapes/LabFirst3DGame.java
@@ -0,0 +1,385 @@
+package com.ru.tgra.shapes;
+
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+
+import java.nio.FloatBuffer;
+
+import com.badlogic.gdx.utils.BufferUtils;
+
+public class LabFirst3DGame extends ApplicationAdapter implements InputProcessor {
+
+	private FloatBuffer matrixBuffer;
+
+	private int renderingProgramID;
+	private int vertexShaderID;
+	private int fragmentShaderID;
+
+	private int positionLoc;
+	private int normalLoc;
+
+	private int modelMatrixLoc;
+	private int viewMatrixLoc;
+	private int projectionMatrixLoc;
+
+	private int colorLoc;
+	
+	private Camera myCamera;
+	
+	Point3D eye;
+	Point3D center;
+	Vector3D upVector;
+	@Override
+	public void create () {
+		
+		Gdx.input.setInputProcessor(this);
+
+		String vertexShaderString;
+		String fragmentShaderString;
+
+		vertexShaderString = Gdx.files.internal("shaders/simple3D.vert").readString();
+		fragmentShaderString =  Gdx.files.internal("shaders/simple3D.frag").readString();
+
+		vertexShaderID = Gdx.gl.glCreateShader(GL20.GL_VERTEX_SHADER);
+		fragmentShaderID = Gdx.gl.glCreateShader(GL20.GL_FRAGMENT_SHADER);
+	
+		Gdx.gl.glShaderSource(vertexShaderID, vertexShaderString);
+		Gdx.gl.glShaderSource(fragmentShaderID, fragmentShaderString);
+	
+		Gdx.gl.glCompileShader(vertexShaderID);
+		Gdx.gl.glCompileShader(fragmentShaderID);
+
+		renderingProgramID = Gdx.gl.glCreateProgram();
+	
+		Gdx.gl.glAttachShader(renderingProgramID, vertexShaderID);
+		Gdx.gl.glAttachShader(renderingProgramID, fragmentShaderID);
+	
+		Gdx.gl.glLinkProgram(renderingProgramID);
+
+		positionLoc				= Gdx.gl.glGetAttribLocation(renderingProgramID, "a_position");
+		Gdx.gl.glEnableVertexAttribArray(positionLoc);
+
+		normalLoc				= Gdx.gl.glGetAttribLocation(renderingProgramID, "a_normal");
+		Gdx.gl.glEnableVertexAttribArray(normalLoc);
+
+		modelMatrixLoc			= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_modelMatrix");
+		viewMatrixLoc			= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_viewMatrix");
+		projectionMatrixLoc	= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_projectionMatrix");
+
+		colorLoc				= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_color");
+
+		Gdx.gl.glUseProgram(renderingProgramID);
+
+		OrthographicProjection3D(0, Gdx.graphics.getWidth(), 0, Gdx.graphics.getHeight(), -2, 2);
+		
+		//COLOR IS SET HERE
+		Gdx.gl.glUniform4f(colorLoc, 0.7f, 0.2f, 0, 1);
+
+		BoxGraphic.create(positionLoc, normalLoc);
+		SphereGraphic.create(positionLoc, normalLoc);
+		SincGraphic.create(positionLoc);
+		CoordFrameGraphic.create(positionLoc);
+
+		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+		ModelMatrix.main = new ModelMatrix();
+		ModelMatrix.main.loadIdentityMatrix();
+		ModelMatrix.main.setShaderMatrix(modelMatrixLoc);
+
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+
+		//OrthographicProjection3D(-2, 2, -2, 2, 1, 100);
+		PerspctiveProjection3D();
+		eye = new Point3D(1.5f, 1.2f, 2.0f);
+		myCamera = new Camera(eye);
+		center = new Point3D(0,0,0);
+		upVector = new Vector3D(0,1,0);
+		myCamera.Look3D(eye, center, upVector);
+		myCamera.setShaderMatrix(viewMatrixLoc);
+	}
+
+	private void input()
+	{
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		float speed = 2 * deltaTime;
+		float angle = 180.0f * deltaTime;
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			myCamera.Slide(-speed, 0, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			myCamera.Slide(speed, 0, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			myCamera.Slide(0, speed, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			myCamera.Slide(0, -speed, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+			myCamera.Roll(angle);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.D)) {
+			myCamera.Roll(-angle);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+			myCamera.Pitch(-angle);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+			myCamera.Pitch(angle);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.Q)) {
+			myCamera.Yaw(-angle);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.E)) {
+			myCamera.Yaw(angle);
+		}
+		myCamera.setShaderMatrix(viewMatrixLoc);
+	}
+	
+	private void update()
+	{
+		//do all updates to the game
+	}
+	
+	private void display()
+	{
+		//do all actual drawing and rendering here
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		Gdx.gl.glUniform4f(colorLoc, 0.9f, 0.3f, 0.1f, 1.0f);
+
+		ModelMatrix.main.loadIdentityMatrix();
+		
+		float space = 0.13f;
+		Point3D pos = new Point3D(0,0,0);
+		
+		drawCubeLine(9, pos, 1, space);
+		drawCubeLine(9, pos, 3, space);
+		drawCubeLine(9, pos, 0, space);
+		drawCubeLine(9, pos, 2, space);
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(8, pos, 1, space);
+		drawCubeLine(8, pos, 3, space);
+		drawCubeLine(8, pos, 0, space);
+		drawCubeLine(8, pos, 2, space);
+		
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(7, pos, 1, space);
+		drawCubeLine(7, pos, 3, space);
+		drawCubeLine(7, pos, 0, space);
+		drawCubeLine(7, pos, 2, space);
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(6, pos, 1, space);
+		drawCubeLine(6, pos, 3, space);
+		drawCubeLine(6, pos, 0, space);
+		drawCubeLine(6, pos, 2, space);
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(5, pos, 1, space);
+		drawCubeLine(5, pos, 3, space);
+		drawCubeLine(5, pos, 0, space);
+		drawCubeLine(5, pos, 2, space);
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(4, pos, 1, space);
+		drawCubeLine(4, pos, 3, space);
+		drawCubeLine(4, pos, 0, space);
+		drawCubeLine(4, pos, 2, space);
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(3, pos, 1, space);
+		drawCubeLine(3, pos, 3, space);
+		drawCubeLine(3, pos, 0, space);
+		drawCubeLine(3, pos, 2, space);
+		
+		pos.x += space / 2;
+		pos.y += space / 1.5;
+		pos.z += space / 2;
+		
+		drawCubeLine(2, pos, 1, space);
+		drawCubeLine(2, pos, 3, space);
+		drawCubeLine(2, pos, 0, space);
+		drawCubeLine(2, pos, 2, space);
+		
+		pos.x += space;
+		pos.y += space / 1.5;
+		pos.z += space;
+		
+		drawCubeLine(1, pos, 1, space);
+		
+		
+		
+		
+	}
+	
+	public void drawCubeLine(int num, Point3D pos, int direction, float space) {
+		for(int i = 0; i < num; i++) {
+			if(direction == -2){
+				drawCubeAtPos(pos);
+				pos.y -= space;
+				
+			} else if(direction == -1) {
+				drawCubeAtPos(pos);
+				pos.y += space;
+				
+			} else if(direction == 0) {
+				drawCubeAtPos(pos);
+				pos.x -= space;
+				
+			} else if(direction == 1) {
+				drawCubeAtPos(pos);
+				pos.x += space;
+				
+			} else if(direction == 2) {
+				drawCubeAtPos(pos);
+				pos.z -= space;
+				
+			} else if(direction == 3) {
+				drawCubeAtPos(pos);
+				pos.z += space;
+				
+			}
+		}
+	}
+	
+	public void drawCubeAtPos(Point3D pos) {
+		ModelMatrix.main.pushMatrix();
+		ModelMatrix.main.addTranslation(pos.x, pos.y, pos.z);
+		ModelMatrix.main.addScale(0.1f, 0.1f, 0.1f);
+		ModelMatrix.main.setShaderMatrix();
+		BoxGraphic.drawSolidCube();
+		ModelMatrix.main.popMatrix();
+	}
+
+	@Override
+	public void render () {
+		//Gdx.input.setCursorCatched(true);
+		
+		input();
+		//put the code inside the update and display methods, depending on the nature of the code
+		update();
+		display();
+
+	}
+
+	
+
+	private void OrthographicProjection3D(float left, float right, float bottom, float top, float near, float far) {
+		float[] pm = new float[16];
+
+		pm[0] = 2.0f / (right - left); pm[4] = 0.0f; pm[8] = 0.0f; pm[12] = -(right + left) / (right - left);
+		pm[1] = 0.0f; pm[5] = 2.0f / (top - bottom); pm[9] = 0.0f; pm[13] = -(top + bottom) / (top - bottom);
+		pm[2] = 0.0f; pm[6] = 0.0f; pm[10] = 2.0f / (near - far); pm[14] = (near + far) / (near - far);
+		pm[3] = 0.0f; pm[7] = 0.0f; pm[11] = 0.0f; pm[15] = 1.0f;
+
+		matrixBuffer = BufferUtils.newFloatBuffer(16);
+		matrixBuffer.put(pm);
+		matrixBuffer.rewind();
+		Gdx.gl.glUniformMatrix4fv(projectionMatrixLoc, 1, false, matrixBuffer);
+
+		pm[0] = 1.0f; pm[4] = 0.0f; pm[8] = 0.0f; pm[12] = 0.0f;
+		pm[1] = 0.0f; pm[5] = 1.0f; pm[9] = 0.0f; pm[13] = 0.0f;
+		pm[2] = 0.0f; pm[6] = 0.0f; pm[10] = 1.0f; pm[14] = 0.0f;
+		pm[3] = 0.0f; pm[7] = 0.0f; pm[11] = 0.0f; pm[15] = 1.0f;
+
+		matrixBuffer = BufferUtils.newFloatBuffer(16);
+		matrixBuffer.put(pm);
+		matrixBuffer.rewind();
+		Gdx.gl.glUniformMatrix4fv(viewMatrixLoc, 1, false, matrixBuffer);
+	}
+
+	private void PerspctiveProjection3D() {
+		float[] pm = new float[16];
+
+		pm[0] = 1.0f; pm[4] = 0.0f; pm[8] = 0.0f; pm[12] = 0.0f;
+		pm[1] = 0.0f; pm[5] = 1.0f; pm[9] = 0.0f; pm[13] = 0.0f;
+		pm[2] = 0.0f; pm[6] = 0.0f; pm[10] = -1.02f; pm[14] = -2.02f;
+		pm[3] = 0.0f; pm[7] = 0.0f; pm[11] = -1.0f; pm[15] = 0.0f;
+
+		matrixBuffer = BufferUtils.newFloatBuffer(16);
+		matrixBuffer.put(pm);
+		matrixBuffer.rewind();
+		Gdx.gl.glUniformMatrix4fv(projectionMatrixLoc, 1, false, matrixBuffer);
+
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		Point3D newCenter = new Point3D(screenX, screenY, 0);
+		//myCamera.Look3D(eye, newCenter, upVector);
+		
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		myCamera.Slide(0, 0, amount);
+		return false;
+	}
+
+
+}
