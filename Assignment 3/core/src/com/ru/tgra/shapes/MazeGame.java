396cbc109759fea0e81a75d2b46e6d7b56a8892e
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-10-12 23:27:23 +0000

    Started work on shader class

--- Assignment 3/core/src/com/ru/tgra/shapes/MazeGame.java
+++ Assignment 3/core/src/com/ru/tgra/shapes/MazeGame.java
@@ -6,30 +6,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
-
 import com.badlogic.gdx.utils.BufferUtils;
-
-import javafx.scene.shape.Box;
 
 public class MazeGame extends ApplicationAdapter implements InputProcessor {
 
 	private FloatBuffer matrixBuffer;
-
-	private int renderingProgramID;
-	private int vertexShaderID;
-	private int fragmentShaderID;
-
-	private int positionLoc;
-	private int normalLoc;
-
-	private int modelMatrixLoc;
-	private int viewMatrixLoc;
-	private int projectionMatrixLoc;
-
-	private int colorLoc;
+	
+	Shader shader;
 	
 	private Maze myMaze;
 	
@@ -38,62 +23,20 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 	@Override
 	public void create () {
 		
-		Gdx.input.setInputProcessor(this);
-
-		String vertexShaderString;
-		String fragmentShaderString;
-
-		vertexShaderString = Gdx.files.internal("shaders/simple3D.vert").readString();
-		fragmentShaderString =  Gdx.files.internal("shaders/simple3D.frag").readString();
-
-		vertexShaderID = Gdx.gl.glCreateShader(GL20.GL_VERTEX_SHADER);
-		fragmentShaderID = Gdx.gl.glCreateShader(GL20.GL_FRAGMENT_SHADER);
-	
-		Gdx.gl.glShaderSource(vertexShaderID, vertexShaderString);
-		Gdx.gl.glShaderSource(fragmentShaderID, fragmentShaderString);
-	
-		Gdx.gl.glCompileShader(vertexShaderID);
-		Gdx.gl.glCompileShader(fragmentShaderID);
+		shader = new Shader();
 		
-		System.out.println(Gdx.gl.glGetShaderInfoLog(vertexShaderID));
-		System.out.println(Gdx.gl.glGetShaderInfoLog(fragmentShaderID));
-
-		renderingProgramID = Gdx.gl.glCreateProgram();
-	
-		Gdx.gl.glAttachShader(renderingProgramID, vertexShaderID);
-		Gdx.gl.glAttachShader(renderingProgramID, fragmentShaderID);
-	
-		Gdx.gl.glLinkProgram(renderingProgramID);
-
-		positionLoc				= Gdx.gl.glGetAttribLocation(renderingProgramID, "a_position");
-		Gdx.gl.glEnableVertexAttribArray(positionLoc);
-
-		normalLoc				= Gdx.gl.glGetAttribLocation(renderingProgramID, "a_normal");
-		Gdx.gl.glEnableVertexAttribArray(normalLoc);
-
-		modelMatrixLoc			= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_modelMatrix");
-		viewMatrixLoc			= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_viewMatrix");
-		projectionMatrixLoc	= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_projectionMatrix");
-
-		colorLoc				= Gdx.gl.glGetUniformLocation(renderingProgramID, "u_color");
-
-		Gdx.gl.glUseProgram(renderingProgramID);
+		Gdx.input.setInputProcessor(this);
 
 		OrthographicProjection3D(0, Gdx.graphics.getWidth(), 0, Gdx.graphics.getHeight(), -2, 2);
 		
 		//COLOR IS SET HERE
-		Gdx.gl.glUniform4f(colorLoc, 0.7f, 0.2f, 0, 1);
-
-		//BoxGraphic.create(positionLoc, normalLoc);
-		SphereGraphic.create(positionLoc, normalLoc);
-		SincGraphic.create(positionLoc);
-		CoordFrameGraphic.create(positionLoc);
-
+		shader.setColor(0.7f, 0.2f, 0, 1);
+		
 		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 		ModelMatrix.main = new ModelMatrix();
 		ModelMatrix.main.loadIdentityMatrix();
-		ModelMatrix.main.setShaderMatrix(modelMatrixLoc);
+		ModelMatrix.main.setShaderMatrix(shader.getModelMatrixPointer());
 
 		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 
@@ -103,11 +46,11 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 		Point3D startingPosition = new Point3D(0.5f, 0.5f, -0.5f);
 		
 		myPlayer = new Player(startingPosition);
-		myPlayer.camera.setShaderMatrix(viewMatrixLoc);
+		myPlayer.camera.setShaderMatrix(shader.getViewMatrixPointer());
 		
-		new Pyramid(positionLoc, normalLoc);
+		new Pyramid(shader.getVertexPointer(), shader.getNormalPointer());
 		
-		myMaze = new Maze(positionLoc, normalLoc);
+		myMaze = new Maze(shader.getVertexPointer(), shader.getNormalPointer());
 		
 		Gdx.input.setCursorCatched(true);
 	}
@@ -129,7 +72,7 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 			myPlayer.goBack(myPlayer.speed * deltaTime);
 		}
 		
-		myPlayer.camera.setShaderMatrix(viewMatrixLoc);
+		myPlayer.camera.setShaderMatrix(shader.getViewMatrixPointer());
 	}
 	
 	private void update()
@@ -217,17 +160,11 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 		//do all actual drawing and rendering here
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		Gdx.gl.glUniform4f(colorLoc, 0.9f, 0.3f, 0.1f, 1.0f);
+		shader.setColor(0.9f, 0.3f, 0.1f, 1.0f);
 
 		ModelMatrix.main.loadIdentityMatrix();
 		
 		myMaze.Draw();
-		
-		ModelMatrix.main.pushMatrix();
-		ModelMatrix.main.addScale(0.1f, 0.1f, 0.1f);
-		ModelMatrix.main.setShaderMatrix();
-		SphereGraphic.drawSolidSphere();
-		ModelMatrix.main.popMatrix();
 	}
 	
 
@@ -253,7 +190,7 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 		matrixBuffer = BufferUtils.newFloatBuffer(16);
 		matrixBuffer.put(pm);
 		matrixBuffer.rewind();
-		Gdx.gl.glUniformMatrix4fv(projectionMatrixLoc, 1, false, matrixBuffer);
+		Gdx.gl.glUniformMatrix4fv(shader.getProjectionMatrixPointer(), 1, false, matrixBuffer);
 
 		pm[0] = 1.0f; pm[4] = 0.0f; pm[8] = 0.0f; pm[12] = 0.0f;
 		pm[1] = 0.0f; pm[5] = 1.0f; pm[9] = 0.0f; pm[13] = 0.0f;
@@ -263,7 +200,7 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 		matrixBuffer = BufferUtils.newFloatBuffer(16);
 		matrixBuffer.put(pm);
 		matrixBuffer.rewind();
-		Gdx.gl.glUniformMatrix4fv(viewMatrixLoc, 1, false, matrixBuffer);
+		Gdx.gl.glUniformMatrix4fv(shader.getViewMatrixPointer(), 1, false, matrixBuffer);
 	}
 
 	private void PerspectiveProjection3D() {
@@ -283,7 +220,7 @@ public class MazeGame extends ApplicationAdapter implements InputProcessor {
 		matrixBuffer = BufferUtils.newFloatBuffer(16);
 		matrixBuffer.put(pm);
 		matrixBuffer.rewind();
-		Gdx.gl.glUniformMatrix4fv(projectionMatrixLoc, 1, false, matrixBuffer);
+		Gdx.gl.glUniformMatrix4fv(shader.getProjectionMatrixPointer(), 1, false, matrixBuffer);
 
 	}
 
